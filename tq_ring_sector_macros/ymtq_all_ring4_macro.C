8698de2529339600b56c541dbaf1c16527cba20a
donald-vangilder <dv549522@ohio.edu>
2025-07-23 14:37:29 -0400

    Add files via upload

---
+++ tq_ring_sector_macros/ymtq_all_ring4_macro.C
@@ -0,0 +1,68 @@
+void ymtq_all_ring4_macro() {
+    TTree* tree = (TTree*)gROOT->FindObject("mul");
+    Long64_t nEntries = tree->GetEntries();
+    tree->SetBranchStatus("*", false);
+
+    std::cout << "All Ring 4 Segments" << std::endl;
+
+    const char* detNames[28];
+    TLeaf* leaves[28];
+    Double_t vmin[28], vmax[28];
+    Double_t fmin = 1e30, fmax = -1e30;
+
+    for (int i = 0; i < 28; ++i) {
+        detNames[i] = Form("yield_tq%02d_r4", i+1);
+        tree->SetBranchStatus(detNames[i], true);
+        TBranch* br = tree->GetBranch(detNames[i]);
+        leaves[i] = br->GetLeaf("hw_sum");
+        vmin[i] = 1e30;
+        vmax[i] = -1e30;
+    }
+
+    for (Long64_t ev = 0; ev < nEntries; ev += 100) {
+        tree->GetEntry(ev);
+        for (int i = 0; i < 28; ++i) {
+            Double_t v = leaves[i]->GetValue();
+            if (std::isnan(v)) continue;
+            if (v < vmin[i]) vmin[i] = v;
+            if (v > vmax[i]) vmax[i] = v;
+            if (v < fmin) fmin = v;
+            if (v > fmax) fmax = v;
+        }
+    }
+
+    for (int i = 0; i < 28; ++i) {
+        std::cout << detNames[i] << ": Min = " << vmin[i] << ", Max = " << vmax[i] << std::endl;
+    }
+
+    std::cout << "First Pass:    Min = " << fmin << ", Max = " << fmax << std::endl;
+
+    Double_t yrange = fmax - fmin;
+    if (yrange < 1e-6) yrange = 1e-6;
+    Double_t ymin = fmin - 0.1*yrange;
+    Double_t ymax = fmax + 0.1*yrange;
+
+    std::cout << "Histogram:     Min = " << ymin << ", Max = " << fmax << endl;
+
+    TH2F* h = new TH2F("ring4a", "All Ring 4 Segments;Detector Number;Yield", 30, -0.5, 29.5, 1000, ymin, ymax);
+
+    double tmin = 1e30;
+    double tmax = -1e30;
+
+    for (Long64_t ev = 0; ev < nEntries; ++ev) {
+        tree->GetEntry(ev);
+        for (int i = 0; i < 28; ++i) {
+            Double_t v = leaves[i]->GetValue();
+            if (std::isnan(v)) continue;
+            h->Fill(i+1, v);
+            if (v < tmin) tmin = v;
+            if (v > tmax) tmax = v;
+        }
+    }
+
+    std::cout << "True Global:   Min = " << tmin << ", Max = " << tmax << std::endl;
+    std::cout << "---------------------------------------------" << endl;
+
+    h->SetStats(false);
+    h->Draw("COLZ");
+}
---
+++ tq_ring_sector_macros/ymtq_open_ring1_macro.C
@@ -0,0 +1,69 @@
+void ymtq_open_ring1_macro() {
+    TTree* tree = (TTree*)gROOT->FindObject("mul");
+    Long64_t nEntries = tree->GetEntries();
+    tree->SetBranchStatus("*", false);
+
+    std::cout << "Open Ring 1" << endl;
+
+    const char* detNames[7];
+    TLeaf* leaves[7];
+    Double_t vmin[7], vmax[7];
+    Double_t fmin =  1e30, fmax = -1e30;
+
+    for (int i = 0; i < 7; ++i) {
+        detNames[i] = Form("yield_tq%02d_r1", 4*i+3);
+        tree->SetBranchStatus(detNames[i], true);
+        TBranch* br = tree->GetBranch(detNames[i]);
+        leaves[i] = br->GetLeaf("hw_sum");
+        vmin[i] =  1e30;
+        vmax[i] = -1e30;
+    }
+
+    for (Long64_t ev = 0; ev < nEntries; ev += 100) {
+        tree->GetEntry(ev);
+        for (int i = 0; i < 7; ++i) {
+            Double_t v = leaves[i]->GetValue();
+            if (std::isnan(v)) continue;
+            if (v < vmin[i]) vmin[i] = v;
+            if (v > vmax[i]) vmax[i] = v;
+            if (v < fmin) fmin = v;
+            if (v > fmax) fmax = v;
+        }
+    }
+
+    for (int i = 0; i < 7; ++i) {
+        std::cout << detNames[i] << ": Min = " << vmin[i] << ", Max = " << vmax[i] << std::endl;
+    }
+
+    std::cout << "First Pass:    Min = " << fmin << ", Max = " << fmax << endl;
+
+    Double_t yrange = fmax - fmin;
+    if (yrange < 1e-6) yrange = 1e-6;
+    Double_t ymin = fmin - 0.5*yrange;
+    Double_t ymax= fmax + 0.5*yrange;
+
+    std::cout << "Histogram:     Min = " << ymin << ", Max = " << fmax << endl;
+
+    TH2F* h = new TH2F("ring1o", "Ring 1 Open Segments;Detector Number;Yield", 9, -0.5, 8.5, 1000, ymin, ymax);
+
+    double tmin = 1e30;
+    double tmax = -1e30;
+
+    for (Long64_t ev = 0; ev < nEntries; ++ev) {
+        tree->GetEntry(ev);
+        for (int i = 0; i < 7; ++i) {
+            Double_t v = leaves[i]->GetValue();
+            if (std::isnan(v)) continue;
+            h->Fill(i+1, v);
+            if (v < tmin) tmin = v;
+            if (v > tmax) tmax = v;
+        }
+    }
+
+
+    std::cout << "True Global:   Min = " << tmin << ", Max = " << tmax << endl;
+    std::cout << "---------------------------------------------" << endl;
+
+    h->SetStats(false);
+    h->Draw("COLZ");
+}
